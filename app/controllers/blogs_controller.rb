aa3d3cbc029525fb210464ca26389551cb8275a7
木下晃兵 <kinoshitakohei@MacBook-Air-2.local>
2019-01-10 20:46:58 +0900

    [Finish]3-3

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -5,9 +5,21 @@ class BlogsController < ApplicationController
   def index
   end
 
-  def new
+  def edit
   end
 
-  def edit
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+        blog = Blog.new(blog_params)
+        blog.save
+        redirect_to blogs_path
+  end
+
+  private
+  def blog_params
+      params.require(:blog).permit(:title, :category, :body)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_10_095014) do
+ActiveRecord::Schema.define(version: 2019_01_10_114553) do
 
   create_table "blogs", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
+    t.string "category"
+    t.text "body"
   end
 
 end
